c0917e91e8d5d5a5b687007fbe648cbcb727ae40
lbittencurt <bittencurt.lucas@outlook.com>
2020-07-21 16:44:03 -0300

    tmp

---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import { 
+  Container,
+  Banner,
+  Avatar,
+  ProfileData,
+  LocationIcon,
+  CakeIcon,
+  Followage
+} from './styles';
+
+const ProfilePage: React.FC = () => {
+  return (
+    <Container>
+      <Banner>
+        <Avatar />
+      </Banner>
+      <ProfileData>
+        {/* <EditButton outlined>Editar perfil</EditButton> */}
+        
+        <h1>Nome Sobrenome</h1>
+        <h2>@nome_sobrenome</h2>
+
+        <p>
+          Developer at <a href="https://google.com">@SomeCompany</a>
+        </p>
+
+        <ul>
+          <li>
+            <LocationIcon />
+            São Paulo, Brasil
+          </li>
+          <li>
+            <CakeIcon />
+            Nascido(a) em 21 de junho de 2001
+          </li>
+        </ul>
+
+        <Followage>
+          <span>
+            seguindo <strong>94</strong>
+          </span>
+          <span>
+            <strong>672 </strong> seguidores
+          </span>
+        </Followage>
+      </ProfileData>
+    </Container>
+  );
+}
+
+export default ProfilePage;
